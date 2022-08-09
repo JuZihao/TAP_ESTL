f158d953615b45bf2be3391e1d5ee15fb5f106a1
LAPTOP-L8HMKNLG\orang <ju.zihaosg@gmail.com>
2022-08-09 15:59:11 +0800

    Feature : Add response utility functions

--- src/main/java/com/esm/common/ResponseUtil.java
+++ src/main/java/com/esm/common/ResponseUtil.java
@@ -1,4 +1,10 @@
 package com.esm.common;
 
+import com.esm.entity.Response;
+
 public class ResponseUtil {
+
+    public static Response createResponse(int statusCode, String statusMessage, Object data){
+        return new Response(statusCode,statusMessage,data);
+    }
 }
